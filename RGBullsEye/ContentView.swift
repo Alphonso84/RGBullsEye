6dc89896cbc82ffb2ff2cccb0fff1c8360e06c9f
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-06-27 12:50:55 -0700

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Alphonso Sensley II on 6/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Alphonso Sensley II on 6/27/20.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
